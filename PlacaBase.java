82845cbad1768ec1cb4251e8ab84499bf9d11742
javiergonzalez02 <javi.galeon2012@gmail.com>
2023-11-27 12:01:53 +0100

    Metodos getter para los atributos de la segunda clase

--- PlacaBase.java
+++ PlacaBase.java
@@ -1,9 +1,25 @@
-public class PlacaBase
-{
-   String modelo;
-   int numeroSerie;
-       public PlacaBase(String nuevoModelo, int nuevoNumeroSerie){
-           modelo = nuevoModelo;
-           numeroSerie = nuevoNumeroSerie;
-       }
+public class PlacaBase {
+    String modelo;
+    int numeroSerie;
+
+    public PlacaBase(String nuevoModelo, int nuevoNumeroSerie) {
+        modelo = nuevoModelo;
+        numeroSerie = nuevoNumeroSerie;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public int getNumeroSerie() {
+        return numeroSerie;
+    }
+
+    public void setModelo(String nModelo) {
+        modelo = nModelo;
+    }
+
+    public void setNumeroSerie(int nNumeroSerie) {
+        numeroSerie = nNumeroSerie;
+    }
 }
