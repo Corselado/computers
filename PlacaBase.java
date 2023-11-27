58bd1816898918744808106852102ace9924df49
IvanSD <ivansd122015@gmail.com>
2023-11-27 11:55:21 +0100

    Commit 1

---
+++ PlacaBase.java
@@ -0,0 +1,9 @@
+public class PlacaBase
+{
+   String modelo;
+   int numeroSerie;
+       public PlacaBase(String nuevoModelo, int nuevoNumeroSerie){
+           modelo = nuevoModelo;
+           numeroSerie = nuevoNumeroSerie;
+       }
+}
